1a3f2406b1b35854a968e9d579795f2a979e8a15
Emily <emilychen@gmail.com>
2016-01-27 08:27:17 +0000

    node.js lab

---
+++ routes/project.js
@@ -0,0 +1,11 @@
+/*
+ * routes/project.js
+ */
+
+exports.viewProject = function(req,res) {
+	var name = req.params.name;
+	console.log("The project name is: " + name);
+	res.render('project', {
+ 		'projectName': name
+	});
+}
